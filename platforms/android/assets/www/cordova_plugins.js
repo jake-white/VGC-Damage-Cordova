e54fe095eea22d51f3346f538b7fc19e78d85f24
jake-white <squirrelboy1225@gmail.com>
2017-10-29 02:28:26 -0400

    Alpha v0.1 release

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.2"
+  "cordova-plugin-whitelist": "1.3.2",
+  "cordova-plugin-extension": "1.5.2"
 };
 // BOTTOM OF METADATA
 });
