6125e963b045dda2e82b5977d025dd2da0119654
Jake White <squirrelboy1225@gmail.com>
2017-10-30 23:36:39 -0400

    Alpha v0.9 - Beta v0.1
    Whoops forgot to upload a few builds. In app purchasing now works, and promoted Alpha v1.3 to Beta v0.1

--- www/billing_res/billing_script.js
+++ www/billing_res/billing_script.js
@@ -1,8 +1,12 @@
+var  NO_ADS_OWNED = false;
+
 // We must wait for the "deviceready" event to fire
 // before we can use the store object.
 document.addEventListener('deviceready', initializeStore, false);
-$('#pro_button').click(function(){
-    store.order("pro");
+$('#noads_button').click(function(){
+    if(!NO_ADS_OWNED) {
+      store.order("noads");
+    }
   });
 function initializeStore() {
 
@@ -13,31 +17,42 @@ function initializeStore() {
     // We register a dummy product. It's ok, it shouldn't
     // prevent the store "ready" event from firing.
     store.register({
-        id:    "com.whitejaa.damage.pro_version",
-        alias: "pro",
-        type: store.NON_RENEWING_SUBSCRIPTION
-    });
-    if(store.get("pro").owned) {
-      console.log("Owned.");
-    }
-    else {
-      console.log("Not owned.");
-      console.log(store.get("pro").owned);
-    }
-
-    // When every goes as expected, it's time to celebrate!
-    // The "ready" event should be welcomed with music and fireworks,
-    // go ask your boss about it! (just in case)
-    store.ready(function() {
-        console.log("\\o/ STORE READY \\o/");
-    });
-
-    store.when("pro").approved(function (order) {
-        console.log("You unlocked the pro version!");
-        order.finish();
+        id:    "no_ads",
+        alias: "noads",
+        type: store.NON_CONSUMABLE
     });
 
     // After we've done our setup, we tell the store to do
     // it's first refresh. Nothing will happen if we do not call store.refresh()
     store.refresh();
+
+
+    // When every goes as expected, it's time to celebrate!
+    // The "ready" event should be welcomed with music and fireworks,
+    // go ask your boss about it! (just in case)
+    store.ready(function() {
+      //store is ready, doing nothing for now
+    });
+
+    store.when("noads").approved(function (order) {
+        NO_ADS_OWNED = true;
+        //alert("You have successfully bought the no ads version. This should take effect immediately, if this does not happen it will take effect when you restart the app.");
+        $("label[for='noads_button']").attr('id', "purchased");
+        $("label[for='noads_button']").text("Purchased :)");
+        $("#noads_button").prop("disabled", true);
+        AdMob.removeBanner();
+        checkAds();
+        order.finish();
+    });
+    store.when("noads").cancelled(function (order) {
+        alert("Thanks for considering! Don't worry, this app will always be free to use.");
+        order.finish();
+    });
+    store.when("noads").error(function (order) {
+        alert("Something seems to have gone wrong with your payment or order! Please try again.");
+        order.finish();
+    });
+
+
+    checkAds();
 }
